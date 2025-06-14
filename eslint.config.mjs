83e648cee526c285d8230d1433824638713cd0bc
cylewaitforit <cyle@rightclickcode.com>
2025-06-14 12:42:51 -0500

    fix: eslint flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import eslintPluginNext from "@next/eslint-plugin-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+  {
+    extends: [
+      eslintPluginNext.flatConfig.coreWebVitals,
+      eslintPluginNext.flatConfig.recommended,
+    ],
+  },
+]);
